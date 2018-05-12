de8a5f57a209e82f101d60003e60dc6571f2cf82
Michaela Olson <mco44@cornell.edu>
2018-05-12 18:47:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
   let newBoi = drivers;
-  newBoi.map(function (drive) { return drive.toLowerCase(); });
+  newBoi.map(function (drive) { 
+    return drive.toLowerCase(); 
+  });
   return newBoi;
 }
