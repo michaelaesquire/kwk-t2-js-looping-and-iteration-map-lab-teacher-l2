3acf88678828e83747ea6d117aa8314750b1c4a3
Michaela Olson <mco44@cornell.edu>
2018-05-12 18:54:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ function nameToAttributes(drivers) {
     let index = drive.indexOf(' ');
     let firstName = drive.slice(0,index);
     let lastName = drive.slice(index+1,drive.length);
-    let returnVal = 1;
+    let returnVal = {};
+    returnVal.["first"] = firstName;
+    returnVal.["last"] = lastName;
 //   let returnVal = {
 //      first:firstName;
 //      last:lastName;
