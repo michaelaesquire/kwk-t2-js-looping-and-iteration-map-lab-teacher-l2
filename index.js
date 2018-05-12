637022e94a7a1b7bc38c4dbe46ff993bf00cd1c6
Michaela Olson <mco44@cornell.edu>
2018-05-12 18:59:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  let newBoi = drivers;
+  let carl = newBoi.map(function (drive) {
+//    console.log(drive.toLowerCase());
+    return drive.toLowerCase();
+  });
+  return carl;
+}
+
+function nameToAttributes(drivers) {
+  let newBoi = drivers;
+  let carl = newBoi.map(function (drive) {
+    let index = drive.indexOf(' ');
+    let first = drive.slice(0,index);
+    let last = drive.slice(index+1,drive.length);
+    let returnVal = {};
+    returnVal.firstName = first;
+    returnVal.lastName = last;
+    return returnVal;
+  });
+  return carl;
+}
+
+function attributesToPhrase(drivers) {
+  let newBoi = drivers;
+  let carl = newBoi.map(function (drive) {
+    named = drive.name;
+    fromed = drive.hometown;
+    return named + " is from " + fromed;
+  });
+  return carl;
+}
